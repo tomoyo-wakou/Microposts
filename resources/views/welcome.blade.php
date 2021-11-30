12b57c40d11de6b8eba1cd57f3622c649f0eb21f
tomoyo.wakou <capchiku-airou@ezweb.ne.jp>
2021-11-30 09:22:53 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,11 @@
-@extends('layouts.app')
+@extends("layouts.app")
 
-@section('content')
+@section("content")
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Microposts</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route("signup.get", "Sign up now", [], ["class" => "btn btn-lg btn-primary"]) !!}
         </div>
     </div>
 @endsection
