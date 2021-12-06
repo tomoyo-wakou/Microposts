3e49e514e22d00367920c23603c6f4cecd8b9165
tomoyo.wakou <capchiku-airou@ezweb.ne.jp>
2021-12-06 14:54:24 +0000

    Profile

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -13,9 +13,9 @@ class MicropostsController extends Controller
             // 認証済みユーザを取得
             $user = \Auth::user();
             // ユーザとフォロー中ユーザの投稿の一覧を作成日時の降順で取得
-            $microposts = $user->feel_microposts()->orderBy("created_at", "desc")->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy("created_at", "desc")->paginate(10);
         
-            $date = [
+            $data = [
                 "user" => $user,
                 "microposts" => $microposts,
             ];
--- resources/views/users/card.blade.php
+++ resources/views/users/card.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-header">
-        <h3class="card-title">{{ $user->name }}</h3>
+        <h3 class="card-title">{{ $user->name }}</h3>
     </div>
     <div class="card-body">
         {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
